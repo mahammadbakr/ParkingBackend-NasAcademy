41edddf410a52d6a45a23b61bd0d6359dc552e01
Muhammad M Bakr <mahammadbakr77@gmail.com>
2021-04-30 15:16:18 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Nas Academy !');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at Port: ${port}/`);
+});
+
+// access .env with => process.env.NODE_ENV 
---
+++ README.md
@@ -0,0 +1,3 @@
+#Nas-Academy-Test
+
+*Here it is One of the most important Tests for Nas Academy..
